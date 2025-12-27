dcbcbb358292cfe6aed615b6411874eab217f6b6
madeaditya02 <imadeaditya4@gmail.com>
2025-12-27 20:06:18 +0800

    Set configuration for deployment to render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Plan;
 use App\Models\User;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         Gate::define('edit-plan', function (User $user, Plan $plan) {
             return $plan->users()->wherePivotNotNull('accepted_at')->wherePivot('user_id', $user->id)->count() > 0;
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('invite-delete-plan', function (User $user, Plan $plan) {
             return $plan->users()->wherePivotNotNull('accepted_at')->wherePivot('role', 'Owner')->wherePivot('user_id', auth()->id())->count() > 0;
         });
+
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
